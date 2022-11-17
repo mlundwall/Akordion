305add78bd93a8c6e4fd3dac42865f8dc50e1f9c
Mads Lundwall <mads@lundwall.dk>
2022-11-17 15:40:55 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,329 @@
+﻿namespace Akordion
+{
+    partial class Akordion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Danskala = new System.Windows.Forms.Button();
+            this.Toneart = new System.Windows.Forms.ComboBox();
+            this.Skalatype = new System.Windows.Forms.ComboBox();
+            this.sharpToner = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Danskala
+            // 
+            this.Danskala.Location = new System.Drawing.Point(15, 50);
+            this.Danskala.Margin = new System.Windows.Forms.Padding(4);
+            this.Danskala.Name = "Danskala";
+            this.Danskala.Size = new System.Drawing.Size(116, 28);
+            this.Danskala.TabIndex = 0;
+            this.Danskala.Text = "&Dan skala";
+            this.Danskala.UseVisualStyleBackColor = true;
+            this.Danskala.Click += new System.EventHandler(this.Danskala_Click);
+            // 
+            // Toneart
+            // 
+            this.Toneart.FormattingEnabled = true;
+            this.Toneart.Location = new System.Drawing.Point(15, 17);
+            this.Toneart.Margin = new System.Windows.Forms.Padding(4);
+            this.Toneart.Name = "Toneart";
+            this.Toneart.Size = new System.Drawing.Size(53, 24);
+            this.Toneart.TabIndex = 4;
+            this.Toneart.Text = "C";
+            this.Toneart.SelectedIndexChanged += new System.EventHandler(this.Toneart_SelectedIndexChanged);
+            // 
+            // Skalatype
+            // 
+            this.Skalatype.FormattingEnabled = true;
+            this.Skalatype.Items.AddRange(new object[] {
+            "Dur",
+            "Mol"});
+            this.Skalatype.Location = new System.Drawing.Point(77, 17);
+            this.Skalatype.Margin = new System.Windows.Forms.Padding(4);
+            this.Skalatype.Name = "Skalatype";
+            this.Skalatype.Size = new System.Drawing.Size(52, 24);
+            this.Skalatype.TabIndex = 5;
+            this.Skalatype.Text = "Dur";
+            this.Skalatype.SelectedIndexChanged += new System.EventHandler(this.Skalatype_SelectedIndexChanged);
+            // 
+            // sharpToner
+            // 
+            this.sharpToner.AutoSize = true;
+            this.sharpToner.Checked = true;
+            this.sharpToner.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.sharpToner.Location = new System.Drawing.Point(155, 20);
+            this.sharpToner.Name = "sharpToner";
+            this.sharpToner.Size = new System.Drawing.Size(36, 20);
+            this.sharpToner.TabIndex = 7;
+            this.sharpToner.Text = "#";
+            this.sharpToner.UseVisualStyleBackColor = true;
+            this.sharpToner.CheckedChanged += new System.EventHandler(this.sharpBox_CheckboxCanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.label1.Location = new System.Drawing.Point(11, 105);
+            this.label1.Margin = new System.Windows.Forms.Padding(0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "-";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(59, 105);
+            this.label2.Margin = new System.Windows.Forms.Padding(0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 22);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "-";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(106, 105);
+            this.label3.Margin = new System.Windows.Forms.Padding(0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 22);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "-";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Crimson;
+            this.label4.Location = new System.Drawing.Point(154, 105);
+            this.label4.Margin = new System.Windows.Forms.Padding(0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(22, 22);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "-";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Crimson;
+            this.label5.Location = new System.Drawing.Point(202, 105);
+            this.label5.Margin = new System.Windows.Forms.Padding(0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(22, 22);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "-";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(250, 105);
+            this.label6.Margin = new System.Windows.Forms.Padding(0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(22, 22);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "-";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(299, 105);
+            this.label7.Margin = new System.Windows.Forms.Padding(0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(22, 22);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "-";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.label8.Location = new System.Drawing.Point(345, 105);
+            this.label8.Margin = new System.Windows.Forms.Padding(0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(22, 23);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "-";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(438, 105);
+            this.label10.Margin = new System.Windows.Forms.Padding(0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(22, 22);
+            this.label10.TabIndex = 22;
+            this.label10.Text = "-";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.ForeColor = System.Drawing.Color.Crimson;
+            this.label11.Location = new System.Drawing.Point(488, 105);
+            this.label11.Margin = new System.Windows.Forms.Padding(0);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(22, 22);
+            this.label11.TabIndex = 21;
+            this.label11.Text = "-";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.ForeColor = System.Drawing.Color.Crimson;
+            this.label12.Location = new System.Drawing.Point(536, 105);
+            this.label12.Margin = new System.Windows.Forms.Padding(0);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(22, 22);
+            this.label12.TabIndex = 20;
+            this.label12.Text = "-";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(587, 105);
+            this.label13.Margin = new System.Windows.Forms.Padding(0);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(22, 22);
+            this.label13.TabIndex = 19;
+            this.label13.Text = "-";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.Location = new System.Drawing.Point(632, 105);
+            this.label14.Margin = new System.Windows.Forms.Padding(0);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(22, 22);
+            this.label14.TabIndex = 18;
+            this.label14.Text = "-";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label15.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.label15.Location = new System.Drawing.Point(683, 105);
+            this.label15.Margin = new System.Windows.Forms.Padding(0);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(22, 23);
+            this.label15.TabIndex = 17;
+            this.label15.Text = "-";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(394, 105);
+            this.label9.Margin = new System.Windows.Forms.Padding(0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(22, 22);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "-";
+            // 
+            // Akordion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(749, 226);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.sharpToner);
+            this.Controls.Add(this.Skalatype);
+            this.Controls.Add(this.Toneart);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Danskala);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Akordion";
+            this.Text = "Akordion";
+            this.Load += new System.EventHandler(this.Akordion_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Danskala;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox Toneart;
+        private System.Windows.Forms.ComboBox Skalatype;
+        private System.Windows.Forms.CheckBox sharpToner;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Akordion
+{
+    public partial class Akordion : Form
+    {
+        readonly String[] Tone = { "C", "Cis", "D", "Dis", "E", "F", "Fis", "G", "Gis", "A", "Ais", "H" };
+        readonly String[] Toneb = { "C", "Des", "D", "Es", "E", "F", "Ges", "G", "As", "A", "Bb", "H" };
+        readonly byte[] Dur = { 2, 2, 1, 2, 2, 2, 1 };
+        readonly byte[] Mol = { 2, 1, 2, 2, 1, 2, 2 };
+        readonly byte halvtoner = 12;
+        readonly byte heltoner = 7;
+
+        public Akordion()
+        {
+            InitializeComponent();
+        }
+
+        public void Visskala()
+        {
+            byte[] skala;
+            System.Windows.Forms.Label[] labels = new System.Windows.Forms.Label[15];
+            labels[0]  = label1;
+            labels[1]  = label2;
+            labels[2]  = label3;
+            labels[3]  = label4;
+            labels[4]  = label5;
+            labels[5]  = label6;
+            labels[6]  = label7;
+            labels[7]  = label8;
+            labels[8]  = label9;
+            labels[9]  = label10;
+            labels[10] = label11;
+            labels[11] = label12;
+            labels[12] = label13;
+            labels[13] = label14;
+            labels[14] = label15;
+            if (Skalatype.SelectedIndex == 0)
+                skala = Dur;
+            else
+                skala = Mol;
+
+            int tpos = 0 + Toneart.SelectedIndex;
+            for (int j = 0; j < 2 * heltoner + 1; j++)
+            {
+                if (sharpToner.Checked)
+                    labels[j].Text = Tone[tpos];
+                else
+                    labels[j].Text = Toneb[tpos];
+                tpos += skala[j % heltoner];
+                tpos = tpos % halvtoner;
+            }
+        }
+        private void Danskala_Click(object sender, EventArgs e)
+        {
+            Visskala();
+        }
+
+        private void Akordion_Load(object sender, EventArgs e)
+        {
+            Skalatype.SelectedIndex = 0;
+            for (int i = 0; i < halvtoner; i++)
+                Toneart.Items.Add(Tone[i]);
+            Toneart.SelectedIndex = 0;
+        }
+
+        private void sharpBox_CheckboxCanged(object sender, EventArgs e)
+        {
+            Danskala_Click(null, null);
+        }
+
+        private void Toneart_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Danskala_Click(null, null);
+        }
+
+        private void Skalatype_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Danskala_Click(null, null);
+        }
+
+    }
+}
